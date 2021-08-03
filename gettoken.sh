73c24d4377bb944b0f48cf770bf5799412fe959e
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2021-08-03 15:06:08 +0430

    Create gettoken.sh

---
+++ gettoken.sh
@@ -0,0 +1,3 @@
+export NAMESPACE="stg"
+export K8S_USER="stg"
+kubectl -n ${NAMESPACE} describe secret $(kubectl -n ${NAMESPACE} get secret | (grep ${K8S_USER} || echo "$_") | awk '{print $1}') | grep token: | awk '{print $2}'\n
