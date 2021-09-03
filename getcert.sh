e6a21cafa65d19372b89e195b4209a68de04bc5e
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2021-09-04 03:39:24 +0430

    Delete getcert.sh

--- getcert.sh
+++
@@ -1,4 +0,0 @@
-NAMESPACE="stg"
-K8S_USER="stg"
-kubectl  -n ${NAMESPACE} get secret `kubectl -n ${NAMESPACE} get secret | (grep ${K8S_USER} || echo "$_") | awk '{print $1}'` -o "jsonpath={.data['ca\.crt']}"
-
