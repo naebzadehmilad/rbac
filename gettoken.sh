dcc8908efacf0e76a940cb010f16d36b674c370c
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2021-09-04 03:39:43 +0430

    Delete gettoken.sh

--- gettoken.sh
+++
@@ -1,3 +0,0 @@
-export NAMESPACE="stg"
-export K8S_USER="stg"
-kubectl -n ${NAMESPACE} describe secret $(kubectl -n ${NAMESPACE} get secret | (grep ${K8S_USER} || echo "$_") | awk '{print $1}') | grep token: | awk '{print $2}'\n
