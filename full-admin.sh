f4b4037087c53c0372608032de270cc6cb7b4c13
Milad.Naebzadeh <45167308+naebzadehmilad@users.noreply.github.com>
2025-06-09 14:02:45 +0330

    Create full-admin.sh

---
+++ full-admin.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+NEW_USER="admin"
+NAMESPACE="kube-system"
+CONFIG_FILE="${NEW_USER}-config.yaml"
+
+kubectl create serviceaccount "${NEW_USER}" -n "${NAMESPACE}"
+
+kubectl create clusterrolebinding "${NEW_USER}-binding" \
+  --clusterrole=cluster-admin \
+  --serviceaccount="${NAMESPACE}:${NEW_USER}"
+
+TOKEN=$(kubectl -n "${NAMESPACE}" create token "${NEW_USER}")
+
+SERVER=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
+CA_DATA=$(kubectl config view --raw --minify -o jsonpath='{.clusters[0].cluster.certificate-authority-data}')
+
+cat <<EOF > "${CONFIG_FILE}"
+apiVersion: v1
+kind: Config
+clusters:
+- name: kubernetes
+  cluster:
+    certificate-authority-data: ${CA_DATA}
+    server: ${SERVER}
+contexts:
+- name: ${NEW_USER}-context
+  context:
+    cluster: kubernetes
+    user: ${NEW_USER}
+current-context: ${NEW_USER}-context
+users:
+- name: ${NEW_USER}
+  user:
+    token: ${TOKEN}
+EOF
+
+echo "Kubeconfig  '${NEW_USER}' : ${CONFIG_FILE}"
